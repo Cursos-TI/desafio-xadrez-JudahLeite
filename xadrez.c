568306ad8e8936f2320bbd5de92cc7fa76aad827
JudahLeite <judahprogramacao@gmail.com>
2025-06-05 04:03:47 +0000

    XADREZ NÍVEL AVENTUREIRO OK

--- xadrez.c
+++ xadrez.c
@@ -2,11 +2,11 @@
 // Desafio de Xadrez - MateCheck
 int main() {
     
-int b, t, r; // variáveis para peças Bispo, Torre, Rainha, respectivamente.
+int b, t, r, c; // variáveis para peças Bispo, Torre, Rainha, cavalo, respectivamente.
 b = 1;
 t = 1;
 r = 1;
-
+c = 1;
     //Bispo
 printf("Movimentação do Bispo:\n");
     while (b<=5){
@@ -24,10 +24,17 @@ printf("Movimentação Rainha:\n");
     for (r; r<=8; r++){
         printf("Esquerda\n");   
     }
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    // Cavalo
+printf("Movimentação do Cavalo:\n");
+    while(c<=1){
+        c++;
+        for (int i = 1; i<=2; i++ ){
+            printf("Baixo\n");
+        }
+        printf("Esquerda\n");
+        
+    }   
+    printf("\n");
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
